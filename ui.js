4073354d1eeb76ca38d8dedd6c3feb97f0b7abbd
Sumit123409 <108711698+Sumit123409@users.noreply.github.com>
2025-08-29 12:24:14 +0530

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,21 @@
+class UI {
+  constructor() {
+    this.scoreElement = document.getElementById("score");
+    this.restartBtn = document.getElementById("restartBtn");
+    this.score = 0;
+  }
+
+  updateScore(value) {
+    this.score = value;
+    this.scoreElement.textContent = "Score: " + this.score;
+  }
+
+  showRestart(callback) {
+    this.restartBtn.style.display = "block";
+    this.restartBtn.onclick = callback;
+  }
+
+  hideRestart() {
+    this.restartBtn.style.display = "none";
+  }
+}
