9b30d67fd68661f4e71c3e4d5df5967a2444b0c5
Sumit <108711698+Sm2101@users.noreply.github.com>
2025-08-30 19:33:37 +0530

    Update road.js

--- road.js
+++ road.js
@@ -4,9 +4,17 @@ class Road {
     for (let i = 0; i < 20; i++) {
       this.lines.push({ x: 195, y: i * 40 });
     }
+    this.bgY = 0;
+    this.bgImage = new Image();
+    this.bgImage.src = "road_bg.png"; // must exist in your folder
   }
 
   draw(ctx) {
+    if (!ctx) return;
+    // Animated background
+    ctx.drawImage(this.bgImage, 0, this.bgY, 400, 600);
+    ctx.drawImage(this.bgImage, 0, this.bgY - 600, 400, 600);
+
     ctx.fillStyle = "white";
     this.lines.forEach(line => {
       ctx.fillRect(line.x, line.y, 10, 20);
@@ -14,9 +22,11 @@ class Road {
   }
 
   update(speed) {
+    this.bgY += speed;
+    if (this.bgY >= 600) this.bgY = 0;
     this.lines.forEach(line => {
       line.y += speed;
-      if (line.y > 600) line.y = 0;
+      if (line.y > 600) line.y -= 600;
     });
   }
 }
